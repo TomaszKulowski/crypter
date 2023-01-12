c06437dfc3f71ec2fa5124b9c672088b3b194f64
Tomasz987 <tomek987125@gmail.com>
2023-01-12 18:13:51 +0100

    Refactor all File class
    Changed the filename argument as property in the File class constructor

--- file_managements.py
+++ file_managements.py
@@ -5,42 +5,45 @@ from json import dump, load
 
 
 class File(ABC):
+    def __init__(self, filename):
+        self.filename = filename
+
     @abstractmethod
-    def load(self, filename):
+    def load(self):
         pass
 
     @abstractmethod
-    def save(self, filename, data):
+    def save(self, data):
         pass
 
 
 class TXTFile(File):
-    def load(self, filename):
-        with open(filename, 'r', encoding='utf-8') as file:
+    def load(self):
+        with open(self.filename, 'r', encoding='utf-8') as file:
             return file.read()
 
-    def save(self, filename, data):
-        with open(filename, 'w', encoding='utf-8') as file:
+    def save(self, data):
+        with open(self.filename, 'w', encoding='utf-8') as file:
             file.write(data)
 
 
 class CRFile(File):
-    def load(self, filename):
-        with open(filename, 'r', encoding='utf-8') as file:
+    def load(self):
+        with open(self.filename, 'r', encoding='utf-8') as file:
             return file.read()
 
-    def save(self, filename, data):
-        with open(filename, 'w', encoding='utf-8') as file:
+    def save(self, data):
+        with open(self.filename, 'w', encoding='utf-8') as file:
             file.write(data)
 
 
 class JSONFile(File):
-    def load(self, filename):
-        with open(filename, 'r', encoding='utf-8') as file:
+    def load(self):
+        with open(self.filename, 'r', encoding='utf-8') as file:
             return file.read()
 
-    def save(self, filename, data):
-        with open(filename, 'w', encoding='utf-8') as file:
+    def save(self, data):
+        with open(self.filename, 'w', encoding='utf-8') as file:
             file.write(data)
 
 
@@ -49,4 +52,7 @@ class FileFactory:
     def get_file(filename):
         _, file_extension = os.path.splitext(filename)
         class_name = getattr(sys.modules[__name__], file_extension[1:].upper() + 'File')
-        class_name().load(filename)
+        return class_name(filename)
+
+
+file = FileFactory.get_file('dupa.cr')
