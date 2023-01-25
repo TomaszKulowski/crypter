f360db2b69cbd9e7074a1869ea40fa1f643c14e1
Tomasz987 <tomek987125@gmail.com>
2023-01-25 20:48:09 +0100

    Create tests for the protection.py module

---
+++ tests/protection_test.py
@@ -0,0 +1,36 @@
+"""The collections of the test for protection module"""
+from cryptography.fernet import Fernet
+
+from tools.protection import Protection
+
+
+def test_encrypt_data(mocker):
+    """Check the data is encrypted correctly.
+
+    Args:
+        mocker (pytest_mock): mock called methods
+    """
+    excepted_result = 'gAAAAABj0YLPHaoW=='
+    mocker.patch.object(Fernet, 'encrypt', return_value=bytes(excepted_result, 'utf-8'))
+    protection = Protection('pass word')
+
+    result = protection.encrypt(' data to encrypt')
+
+    assert result == excepted_result
+    Fernet.encrypt.assert_called_once()
+
+
+def test_decrypt_data(mocker):
+    """Check the encrypted data is decrypted correctly.
+
+    Args:
+        mocker (pytest_mock): mock called methods
+    """
+    excepted_result = 'decrypted data '
+    mocker.patch.object(Fernet, 'decrypt', return_value=bytes(excepted_result, 'utf-8'))
+    protection = Protection('Password!@#12 ')
+
+    result = protection.decrypt('gAAAAABj0YLPHaoW==')
+
+    assert result == excepted_result
+    Fernet.decrypt.assert_called_once()
