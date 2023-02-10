869d6637ee45339f4d515fa286b1566b524b67c3
Tomasz987 <tomek987125@gmail.com>
2023-02-10 20:16:37 +0100

    Add progress bar support

--- tools/file_management.py
+++ tools/file_management.py
@@ -1,4 +1,7 @@
 """The collections to operate on files."""
+from os.path import getsize
+
+from tools.progress_bar import ProgressBar
 
 
 class File:
@@ -12,12 +15,30 @@ class File:
         """Construct all the necessary attributes for the file object"""
         self.file_path = file_path
 
+    @staticmethod
+    def _strip_new_line_character(line):
+        """Remove a new line character.
+        The 10 number is a new line character in bytes."""
+        if line[-1] == 10:
+            return line[:-1]
+        return line
+
     def load(self) -> str:
         """Open and return data from the file."""
-        with open(self.file_path, 'r', encoding='utf-8') as file:
-            return file.read()
+        result = b''
+        file_size = getsize(self.file_path)
+        progress_bar = ProgressBar(self.load.__name__, file_size)
+        with open(self.file_path, 'rb') as file:
+            while file.tell() != file_size:
+                data = self._strip_new_line_character(file.readline())
+                result += data
+                progress_bar.set(file.tell())
+            return result.decode('utf-8')
 
     def save(self, data: str):
         """Open and save passed data to the file."""
+        progress_bar = ProgressBar(self.save.__name__, len(data))
         with open(self.file_path, 'w', encoding='utf-8') as file:
-            file.write(data)
+            for index, byte in enumerate(data, 1):
+                progress_bar.set(index)
+                file.write(byte)
