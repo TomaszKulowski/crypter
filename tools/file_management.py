b2c14e4fe34e931facdee4ebb51789957a2f186b
Tomasz987 <tomek987125@gmail.com>
2023-02-14 19:17:18 +0100

    Add progress bar support and file_path getter and setter

--- tools/file_management.py
+++ tools/file_management.py
@@ -1,5 +1,5 @@
 """The collections to operate on files."""
-from os.path import getsize
+from os import path
 
 from tools.progress_bar import ProgressBar
 
@@ -8,36 +8,51 @@ class File:
     """Create object with path file.
 
     Methods:
+        file_path: get current file path
+        file_path(file_path: str): set new file path
         load(): return data from file
         save(data: str): save passed data to file
     """
-    def __init__(self, file_path: str):
+    def __init__(self):
         """Construct all the necessary attributes for the file object"""
-        self.file_path = file_path
+        self._file_path = None
+
+    @property
+    def file_path(self) -> str:
+        """Get or set file path."""
+        return self._file_path
+
+    @file_path.setter
+    def file_path(self, file_path: str):
+        if path.isfile(file_path):
+            self._file_path = file_path
+        else:
+            raise FileNotFoundError('File not found')
 
     @staticmethod
-    def _strip_new_line_character(line):
+    def _strip_last_new_line_character(line: bytes) -> bytes:
         """Remove a new line character.
         The 10 number is a new line character in bytes."""
-        if line[-1] == 10:
-            return line[:-1]
+        if line:
+            if line[-1] == 10:
+                return line[:-1]
         return line
 
     def load(self) -> str:
         """Open and return data from the file."""
         result = b''
-        file_size = getsize(self.file_path)
-        progress_bar = ProgressBar(self.load.__name__, file_size)
+        file_size = path.getsize(self.file_path)
+        progress_bar = ProgressBar('load', file_size)
         with open(self.file_path, 'rb') as file:
             while file.tell() != file_size:
-                data = self._strip_new_line_character(file.readline())
+                data = self._strip_last_new_line_character(file.readline())
                 result += data
                 progress_bar.set(file.tell())
             return result.decode('utf-8')
 
     def save(self, data: str):
         """Open and save passed data to the file."""
-        progress_bar = ProgressBar(self.save.__name__, len(data))
+        progress_bar = ProgressBar('save', len(data))
         with open(self.file_path, 'w', encoding='utf-8') as file:
             for index, byte in enumerate(data, 1):
                 progress_bar.set(index)
