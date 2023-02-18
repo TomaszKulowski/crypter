391dcb078045644b2bf8df90bef5498ce009d8f3
Tomasz987 <tomek987125@gmail.com>
2023-02-18 18:08:34 +0100

    Add setter and getter for the progress bar

--- tools/progress_bar.py
+++ tools/progress_bar.py
@@ -6,14 +6,24 @@ class ProgressBar:
     """Create a progress bar.
 
     Methods:
-        set(current_byte: str): set the progress bar status to the passed value
+        progress: get the current progress of the progress bar
+        progress(current_byte: str): set the progress bar status to the passed value
     """
     def __init__(self, event_name: str, file_size: int):
         """Construct all the necessary attributes for the file object"""
         print(f'{event_name} data')
-        self.progress_bar = tqdm(total=file_size)
+        self._progress = None
+        self.file_size = file_size
+        self.progress_bar = tqdm(total=self.file_size)
 
-    def set(self, current_byte: int):
-        """Set the progress bar status to the passed value."""
+    @property
+    def progress(self):
+        """Get or set current progress"""
+        return self._progress
+
+    @progress.setter
+    def progress(self, current_byte: int):
+        if 0 > current_byte or current_byte > self.file_size:
+            raise ValueError('incorrect progress bar value')
         self.progress_bar.n = current_byte
         self.progress_bar.refresh()
